7112d043230c7e050c4962d2155c2befb83bfc0f
Fransuelton <elton6103@gmail.com>
2024-02-08 10:50:56 -0300

    feat: Add vote validation and update vote record

--- src/http/routes/vote-on-poll.ts
+++ src/http/routes/vote-on-poll.ts
@@ -18,6 +18,29 @@ export async function voteOnPoll(app: FastifyInstance) {
 
     let { sessionId } = request.cookies;
 
+    if (sessionId) {
+      const userPreviousVoteOnPoll = await prisma.vote.findUnique({
+        where: {
+          sessionId_pollId: {
+            sessionId,
+            pollId,
+          },
+        },
+      });
+
+      if (userPreviousVoteOnPoll && userPreviousVoteOnPoll.pollOptionId !== pollOptionId) {
+
+        await prisma.vote.delete({
+          where: {
+            id: userPreviousVoteOnPoll.id,
+          }
+        })
+        
+      } else if (userPreviousVoteOnPoll) {
+        return reply.status(400).send({ message: "You already voted on this poll.",});
+      }
+    }
+
     if (!sessionId) {
       sessionId = randomUUID();
 
@@ -29,6 +52,14 @@ export async function voteOnPoll(app: FastifyInstance) {
       });
     }
 
-    return reply.status(201).send({sessionId});
+    await prisma.vote.create({
+      data: {
+        sessionId,
+        pollId,
+        pollOptionId,
+      },
+    });
+
+    return reply.status(201).send();
   });
 }
